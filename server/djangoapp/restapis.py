3666b4cd9beaf06cc2a4c3685241792a15a7324f
old-coder77 <asally@gmail.com>
2023-09-07 11:25:01 -0400

    week 3 updates

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -42,17 +42,19 @@ def get_dealers_from_cf(url, **kwargs):
 
     if json_result:
         # Get the row list in JSON as dealers
-        dealers = json_result["body"]["rows"]
+        print(json_result)
+        dealers = json_result
         # For each dealer object
         for dealer in dealers:
             # Get its content in `doc` object
-            dealer_doc = dealer["doc"]
+            dealer_doc = dealer
             # print(dealer_doc)
             # Create a CarDealer object with values in `doc` object
             dealer_obj = CarDealer(address=dealer_doc["address"], city=dealer_doc["city"],
-                                   id=dealer_doc["id"], lat=dealer_doc["lat"], long=dealer_doc["long"],
-                                
-                                   st=dealer_doc["st"], zip=dealer_doc["zip"])
+                                   full_name=dealer_doc["full_name"],id=dealer_doc["id"], 
+                                   lat=dealer_doc["lat"], long=dealer_doc["long"],
+                                   short_name=dealer_doc["short_name"],st=dealer_doc["st"], 
+                                   zip=dealer_doc["zip"])
             results.append(dealer_obj)
 
     return results
